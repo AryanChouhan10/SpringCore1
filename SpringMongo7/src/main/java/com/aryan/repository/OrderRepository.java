a76bbb688d93e4f0b12717455ebe3627342da35d
AryanChouhan10 <92908961+AryanChouhan10@users.noreply.github.com>
2021-11-30 10:52:55 +0530

    Delete SpringMongo7 directory

--- SpringMongo7/src/main/java/com/aryan/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aryan.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.aryan.model.Order;
-
-public interface OrderRepository extends MongoRepository<Order,Integer> {
-
-}
